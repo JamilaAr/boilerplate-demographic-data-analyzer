8df38b4e57dbc494d1bac19fa0389f879ae1a318
Jamila Arabat <jamilaarabat93@gmail.com>
2024-12-19 03:15:05 +0000

    Completed demographic data analyzer calculations

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -1,47 +1,58 @@
 import pandas as pd
 
-
 def calculate_demographic_data(print_data=True):
     # Read data from file
-    df = None
+    df = pd.read_csv('adult.data.csv')
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = None
+    race_count = df['race'].value_counts()
 
     # What is the average age of men?
-    average_age_men = None
+    average_age_men = round(df[df["sex"] == 'Male']['age'].mean(), 1)
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
+    bachelors_count = df['education'].value_counts()['Bachelors']
+    total_count = len(df)
+    percentage_bachelors = round((bachelors_count / total_count) * 100, 1)
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
 
     # with and without `Bachelors`, `Masters`, or `Doctorate`
-    higher_education = None
-    lower_education = None
+    advanced_education = df['education'].isin(['Bachelors', 'Masters', 'Doctorate'])
+    advanced_education_rich = df[advanced_education & (df['salary'] == '>50K')]
+    percentage_advanced_education_rich = round((len(advanced_education_rich) / len(df[advanced_education])) * 100, 1)
 
     # percentage with salary >50K
-    higher_education_rich = None
-    lower_education_rich = None
+    no_advanced_education = ~advanced_education
+    no_advanced_education_rich = df[no_advanced_education & (df['salary'] == '>50K')]
+    percentage_non_advanced_education_rich = round((len(no_advanced_education_rich) / len(df[no_advanced_education])) * 100, 1)
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
-    min_work_hours = None
+    min_work_hours = df["hours-per-week"].min()
 
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
-    num_min_workers = None
-
-    rich_percentage = None
+    min_workers = df['hours-per-week'] == min_work_hours
+    min_workers_rich = df[min_workers & (df['salary'] == '>50K')]
+    percentage_min_workers_rich = round((len(min_workers_rich) / len(df[min_workers])) * 100, 1)
 
     # What country has the highest percentage of people that earn >50K?
-    highest_earning_country = None
-    highest_earning_country_percentage = None
+    country_rich_percentage = (df[df['salary'] == '>50K']['native-country'].value_counts() / 
+                               df['native-country'].value_counts()) * 100
+    country_rich_percentage = country_rich_percentage.dropna().sort_values(ascending=False)
+    highest_earning_country = country_rich_percentage.index[0]
+    highest_earning_country_percentage = round(country_rich_percentage.iloc[0], 1)
 
     # Identify the most popular occupation for those who earn >50K in India.
-    top_IN_occupation = None
+    india_rich = df[(df['native-country'] == 'India') & (df['salary'] == '>50K')]
+    top_IN_occupation = india_rich['occupation'].value_counts().idxmax()
+
+    # Additional variables for print and return
+    higher_education_rich = percentage_advanced_education_rich
+    lower_education_rich = percentage_non_advanced_education_rich
+    rich_percentage = percentage_min_workers_rich
 
     # DO NOT MODIFY BELOW THIS LINE
-
     if print_data:
         print("Number of each race:\n", race_count) 
         print("Average age of men:", average_age_men)
@@ -63,7 +74,6 @@ def calculate_demographic_data(print_data=True):
         'min_work_hours': min_work_hours,
         'rich_percentage': rich_percentage,
         'highest_earning_country': highest_earning_country,
-        'highest_earning_country_percentage':
-        highest_earning_country_percentage,
+        'highest_earning_country_percentage': highest_earning_country_percentage,
         'top_IN_occupation': top_IN_occupation
     }
